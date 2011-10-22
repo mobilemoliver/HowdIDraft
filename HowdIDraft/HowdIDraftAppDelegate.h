b5cfa6309f4b69b25b37da054df2c63700800f63
Mike Oliver <mobilemoliver@gmail.com>
2011-10-22 19:25:26 -0400

    Initial Commit

---
+++ HowdIDraft/HowdIDraftAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HowdIDraftAppDelegate.h
+//  HowdIDraft
+//
+//  Created by Mike Oliver on 10/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HowdIDraftAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
